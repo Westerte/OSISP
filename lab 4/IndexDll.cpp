4ade75871f2b50e6397eb13b8a5e2141a4de140b
Westerte <WesterteN@mail.ru>
2014-11-28 20:32:44 +0300

    Added shared message

--- lab 4/IndexDll.cpp
+++ lab 4/IndexDll.cpp
@@ -30,10 +30,14 @@ typedef struct TT
 	TNode* root;
 };
 
+
+
 TT* BTree = new TT;
 void CreateBTree(TT* T);
 void BTreeInsert(TT *T, TKey* k);
 void BTreeSearch(TNode* x, char* key, TNode* resultNode, int* resultIndex);
+
+
 extern "C"__declspec(dllexport) void __stdcall CreateIndex()
 {
 	CreateBTree(BTree);
@@ -68,7 +72,7 @@ void CreateBTree(TT* T)
 	T->root = newNode;
 }
 
-void BTreeSplitChild(TNode* x, int i)										
+void BTreeSplitChild(TNode* x, int i)
 {
 	TNode* z = new TNode;
 	TNode* y = x->childrenArray[i];
@@ -164,4 +168,108 @@ void BTreeSearch(TNode* x, char* key, TNode* resultNode, int* resultIndex)
 	}
 }
 
+int BTreeDelete(TNode* x, char* key)
+{
+	int i = x->keyNumber - 1;
+	while (i >= 0 && strcmp(key, x->keyArray[i].key) < 0)
+		i--;
+	if (x->isLeaf == true)
+	{
+		if (i >= 0 && strcmp(key, x->keyArray[i].key) == 0)
+		{
+			delete &x->keyArray[i];
+			while (i < x->keyNumber - 1)
+			{
+				x->keyArray[i] = x->keyArray[i + 1];
+				i++;
+			}
+			x->keyNumber--;
+		}
+		else
+			return 0;
+	}
+	else
+	{
+		if (i >= 0 && strcmp(key, x->keyArray[i].key) == 0)
+		{
+			TNode* y = x->childrenArray[i];
+			TNode* z = x->childrenArray[i + 1];
+			if (x->childrenArray[i]->keyNumber > t - 1)
+			{
+				x->keyArray[i] = y->keyArray[y->keyNumber - 1];
+				BTreeDelete(y, key);
+			}
+			else
+			{
+				if (z->keyNumber > t - 1)
+				{
+					x->keyArray[i+1] = z->keyArray[0];
+					BTreeDelete(z, key);
+				}
+				else
+				{
+					y->keyArray[y->keyNumber] = x->keyArray[i];
+					int j;
+					for (j = 0; j < z->keyNumber;j++)
+					{
+						y->keyArray[y->keyNumber + 1 + j] = z->keyArray[j];
+						y->childrenArray[y->keyNumber + 1 + j] = z->childrenArray[j];
+					}
+					y->childrenArray[y->keyNumber + 2 + j] = z->childrenArray[j + 1];
+					y->keyNumber = 2 * t - 1;
+					//delete z;
+					for (int j = i; j < x->keyNumber - 1;j++)
+						x->keyArray[j] = x->keyArray[j + 1];
+					for (int j = i + 1; j < x->keyNumber; j++)
+						x->childrenArray[j] = x->childrenArray[j + 1];
+					x->keyNumber--;
+					BTreeDelete(y, key);
+				}
+			}
+		}
+		else
+		{
+			if (x->childrenArray[i + 1]->keyNumber == t - 1)
+			{
+
+				if (i > 0 && x->childrenArray[i]->keyNumber > t - 1)
+				{ 
+					for (int j = x->childrenArray[i + 1]->keyNumber - 1; j <= 0; j--)
+						x->childrenArray[i + 1]->keyArray[j] = x->childrenArray[i + 1]->keyArray[j + 1];
+					for (int j = x->childrenArray[i + 1]->keyNumber; j <= 0; j--)
+						x->childrenArray[i + 1]->childrenArray[j] = x->childrenArray[i + 1]->childrenArray[j + 1];
+					x->childrenArray[i + 1]->keyArray[0] = x->keyArray[i];
+					x->childrenArray[i + 1]->childrenArray[0] = x->childrenArray[i]->childrenArray[x->childrenArray[i]->keyNumber];
+					x->keyArray[i] = x->childrenArray[i]->keyArray[x->childrenArray[i]->keyNumber - 1];
+					//delete  x->childrenArray[i]->childrenArray[x->childrenArray[i]->keyNumber];
+					x->childrenArray[i]->keyNumber--;
+					x->childrenArray[i+1]->keyNumber++;
+				}
+				else
+				{
+					if (i < 2*t-1 && x->childrenArray[i + 2]->keyNumber > t - 1)
+					{
+						x->childrenArray[i + 1]->keyArray[x->childrenArray[i + 1]->keyNumber] = x->keyArray[i+1];
+						x->childrenArray[i + 1]->childrenArray[x->childrenArray[i + 1]->keyNumber + 1] = x->childrenArray[i+2]->childrenArray[0];
+						x->keyArray[i + 1] = x->childrenArray[i + 2]->keyArray[0];
+						//delete x->childrenArray[i + 2]->childrenArray[0];
+						for (int j = 0; j < x->childrenArray[i + 2]->keyNumber - 1; j--)
+							x->childrenArray[i + 2]->keyArray[j] = x->childrenArray[i + 2]->keyArray[j + 1];
+						for (int j = 0; j < x->childrenArray[i + 2]->keyNumber; j--)
+							x->childrenArray[i + 2]->childrenArray[j] = x->childrenArray[i + 2]->childrenArray[j + 1];
+						x->childrenArray[i + 2]->keyNumber--;
+						x->childrenArray[i + 1]->keyNumber++;
+					}
+					else
+					{
+						
+					}
+				}
+			}
+			else
+				BTreeDelete(x->childrenArray[i], key);
+		}
+	}
+	return 1;
+}
 
